05d12297b6c91062792b3467576eef4404a20562
Eric-Schecter <eric199002@icloud.com>
2022-12-08 16:46:02 +0800

    add react-router to webgl part

--- rasterization/webgl/src/App/index.tsx
+++ rasterization/webgl/src/App/index.tsx
@@ -1,19 +1,23 @@
 import React, { useRef, useEffect } from 'react';
+import { Application } from './gl';
 import styles from './index.module.scss';
-import { Triangle } from './triangle';
 
-export const App = () => {
+type Props = {
+  GL_App:new (container:HTMLDivElement) => Application
+}
+
+export const App = ({GL_App}:Props) => {
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (!ref.current) { return }
-    const app = new Triangle(ref.current);
+    const app = new GL_App(ref.current);
     try {
       app.run();
     } catch (error) {
       console.log(error);
     }
     return ()=>app.cleanup();
-  }, [ref])
+  }, [ref,GL_App])
 
   return <div
     ref={ref}
--- rasterization/webgl/src/index.tsx
+++ rasterization/webgl/src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.module.scss';
 import * as serviceWorker from './serviceWorker';
 import { App } from './App';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import { Triangle } from './App/triangle';
+
+const routes = [
+  // { path: '/', app: Triangle },
+  { path: '/triangle', app: Triangle },
+]
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Switch>
+        {routes.map((d, i) => <Route key={d.path+i} path={d.path} render={() => <App GL_App={d.app} />} />)}
+      </Switch>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
